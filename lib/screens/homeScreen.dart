d4d11c9cecf5dc17f1d3ce3c248863b26e8ba98e
temuulino <temka348@gmail.com>
2024-04-09 12:12:26 +0800

    homescreen api

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,13 +1,61 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:upoint_purchase_app/services/itemService.dart';
+import 'package:upoint_purchase_app/services/purchaseService.dart';
+
+import '../services/meService.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  final String token;
+  const HomeScreen({super.key, required this.token});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final dio = Dio();
+  late Map<String, dynamic> meInfo = {};
+  late Map<String, dynamic> itemInfo = {};
+  late Map<String, dynamic> purchase = {};
+  late String itemId;
+
+  Future<void> fetchMe() async {
+    try {
+      final tempMeService = meService(dio, widget.token);
+      final pMeService = await tempMeService.getMeService();
+
+      meInfo = pMeService;
+      print("success me $meInfo");
+    } catch (error) {
+      print("error: $error");
+    }
+  }
+
+  Future<void> fetchItem() async {
+    try {
+      final tempItemService = itemService(dio, widget.token);
+      final pitemService = await tempItemService.getItemService();
+
+      itemInfo = pitemService;
+      print("Success item: $itemInfo");
+    } catch (error) {
+      print("error: $error");
+    }
+  }
+
+  Future<void> makePurchase() async {
+    try {
+      final tempPurchaseService = purchaseService(dio, widget.token);
+      final ppurchaseService = await tempPurchaseService.doPurchase(itemId);
+
+      purchase = ppurchaseService;
+      print("Success purchase: $purchase");
+    } catch (error) {
+      print("error: $error");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/services/purchaseService.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+
+class purchaseService {
+  final Dio _dio;
+  final String _token;
+
+  purchaseService(this._dio, this._token);
+
+  // TODO device code оруулж ирэх
+  Future<Map<String, dynamic>> doPurchase(
+    String itemId,
+  ) async {
+    try {
+      final response = await _dio.post(
+        'https://9a60-202-131-242-131.ngrok-free.app/auth/purchase',
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer $_token',
+            'Accept': 'application/json',
+          },
+        ),
+        data: {
+          'itemId': itemId,
+        },
+      );
+
+      print("This is purchase service: $response");
+
+      final Map<String, dynamic> purchase = Map<String, dynamic>.from(response.data);
+
+      return purchase;
+    } catch (error) {
+      print('Purchase failed: $error');
+      throw error;
+    }
+  }
+}
